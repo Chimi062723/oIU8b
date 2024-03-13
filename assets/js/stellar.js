b60b507030cdd9efd15cddc017eef079cf292d64
chimi <chimi9391@outlook.com>
2024-03-13 22:26:27 +0800

    add

---
+++ assets/js/stellar.js
@@ -0,0 +1,17 @@
+function getRandomStar(count, maxArea, minArea, starSize) {
+  let boxShadowStyle = (minArea + Math.floor(Math.random()*maxArea))+ 'px ' + (minArea + Math.floor(Math.random()*maxArea)) + 'px ' + 0 + ' ' + starSize + ' ' + 'rgba(255, 255, 255, ' + Math.random() + ')';
+  for(let i=0; i< count; i++) {
+    boxShadowStyle = boxShadowStyle + ', ' + (minArea + Math.floor(Math.random()*maxArea))+ 'px ' + (minArea + Math.floor(Math.random()*maxArea)) + 'px ' + 0 + ' ' + starSize + ' ' + 'rgba(255, 255, 255, ' + Math.random() + ')';
+  }
+  return boxShadowStyle
+}
+
+export function $() {
+  let style = document.createElement('style');
+  document.head.appendChild(style);
+  let sheet = style.sheet;
+  let asteroidBeltStars = getRandomStar(1000, 3000, -290/2, '-104px');
+  sheet.insertRule('.asteroid-belt::before {box-shadow: ' + asteroidBeltStars + '}');
+  let wholeBackgroundStars = getRandomStar(1080, 1920, 0, 0);
+  sheet.insertRule('.solar-syst::after { box-shadow: ' + wholeBackgroundStars + ' }');
+}
